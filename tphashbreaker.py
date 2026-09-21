886a4a1a61c4d8ab5f3c9eddd419d5a77affa8d0
Cherif <Cherif@MacBook-Air.local>
2026-09-21 13:44:58 +0200

    Ajout du craqueur naif avec son hash

--- tphashbreaker.py
+++ tphashbreaker.py
@@ -1,19 +1,24 @@
+import hashlib
+
 alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 mot = "z3D"
+hash_cible = hashlib.sha256(mot.encode()).hexdigest()
 
-def chercher(mot):
+
+def chercher(hash_cible):
     for i in range(len(alphabet)):
         for j in range(len(alphabet)):
             for k in range(len(alphabet)):
                 guess = alphabet[i] + alphabet[j] + alphabet[k]
-                if guess == mot:
-                    print(f"Found the password: {guess}")
+                hash_guess = hashlib.sha256(guess.encode()).hexdigest()
+                if hash_guess == hash_cible:
+                    print(f"Mot de passe trouvé: {guess}")
                     return guess
     return None
 
 
 if __name__ == "__main__":
-    result = chercher(mot)
+    result = chercher(hash_cible)
     if result is None:
-        print("Password not found.")
+        print("Mot de passe non trouvé.")
 
